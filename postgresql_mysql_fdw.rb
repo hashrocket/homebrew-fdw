7d13ea022be25db00f0609bf7cab86b25c4352fa
Joshua Davey <josh@joshuadavey.com>
2013-02-06 17:28:31 -0600

    Ensure we always use homebrew's pg_config

--- postgresql_mysql_fdw.rb
+++ postgresql_mysql_fdw.rb
@@ -1,37 +1,20 @@
 require 'formula'
 
-class MySqlInstalled < Requirement
-  fatal true
-
-  satisfy { which 'mysql_config' }
-
-  def message; <<-EOS.undent
-    MySQL is required to install.
-
-    You can install this with Homebrew using:
-      brew install mysql
-    EOS
-  end
-end
-
 class PostgresqlMysqlFdw < Formula
   version '1.0'
   homepage 'https://github.com/dpage/mysql_fdw'
   url 'https://github.com/dpage/mysql_fdw/archive/4c48adf0f9770722484c8b8dc4d7699d87044c09.tar.gz'
   sha1 '21b3c0e6156a7c3874a14e4ae056322b89fa6a7a'
 
-  depends_on MySqlInstalled
+  depends_on :mysql
   depends_on :postgresql
   depends_on 'cmake' => :build
 
-  def postgresql
-    # Follow the PostgreSQL linked keg back to the active Postgres installation
-    # as it is common for people to avoid upgrading Postgres.
-    Formula.factory('postgresql').linked_keg.realpath
-  end
-
   def install
-    ENV["USE_PGXS"] = "1"
+    ENV.append("USE_PGXS", "1")
+
+    # required to avoid grabbing the system version of pg_config
+    ENV["PATH"] = ["#{`brew --prefix`.chomp}/bin", ENV["PATH"]].join(":")
 
     system "make"
     system "make install"
