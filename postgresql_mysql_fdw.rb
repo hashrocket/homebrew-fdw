9b233137634029989e57c5911fb9e48298f68f67
Joshua Davey <josh@joshuadavey.com>
2013-02-07 09:49:34 -0600

    Use the postgis pattern
    This requires the newest version of postgres to install, but does not
    require it to be the active version. Instead, it will install to the
    currently selected postgres.

--- postgresql_mysql_fdw.rb
+++ postgresql_mysql_fdw.rb
@@ -7,16 +7,35 @@ class PostgresqlMysqlFdw < Formula
   sha1 '21b3c0e6156a7c3874a14e4ae056322b89fa6a7a'
 
   depends_on :mysql
-  depends_on :postgresql
+  depends_on 'postgresql'
   depends_on 'cmake' => :build
+
+  def postgresql
+    # Follow the PostgreSQL linked keg back to the active Postgres installation
+    # as it is common for people to avoid upgrading Postgres.
+    Formula.factory('postgresql').linked_keg.realpath
+  end
 
   def install
     ENV.append("USE_PGXS", "1")
 
-    # required to avoid grabbing the system version of pg_config
-    ENV["PATH"] = ["#{`brew --prefix`.chomp}/bin", ENV["PATH"]].join(":")
-
     system "make"
-    system "make install"
+
+    # mysql_fdw includes the PGXS makefiles and so will install __everything__
+    # into the Postgres keg instead of the mysql_fdw keg. Unfortunately, some
+    # things have to be inside the Postgres keg in order to be function. So, we
+    # install everything to a staging directory and manually move the pieces
+    # into the appropriate prefixes.
+    mkdir 'stage'
+    system 'make', 'install', "DESTDIR=#{buildpath}/stage"
+
+    so = Dir['stage/**/*.so']
+    extensions = Dir['stage/**/extension/*']
+
+    (postgresql/'lib').install so
+
+    # Install extension scripts to the Postgres keg.
+    # `CREATE EXTENSION mysql_fdw;` won't work if these are located elsewhere.
+    (postgresql/'share/postgresql/extension').install extensions
   end
 end
